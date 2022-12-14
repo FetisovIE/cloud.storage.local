0fa3b113954b7474f6b679de1656d45fc18f4021
Fetisov Igor <ig.fetisoff@gmail.com>
2022-12-15 00:43:10 +0300

    Configuring routes

---
+++ cloud-storage/routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+Route::post('/user', [UserController::class, 'create']);
+Route::delete('/user/{id}', [UserController::class, 'delete']);
+Route::put('/user', [UserController::class, 'update']);
+Route::get('/user', [UserController::class, 'listUsers']);
+Route::get('/user/{id}', [UserController::class, 'listUser']);
+
+Route::post('/login', [UserController::class, 'login'])->name('login');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
+Route::group([
+    'prefix' => 'admin'
+], function () {
+    Route::get('user/{id}', [AdminController::class, 'listUser']);
+    Route::get('user', [AdminController::class, 'listUsers']);
+    Route::delete('user/{id}', [AdminController::class, 'delete']);
+    Route::put('user', [AdminController::class, 'update']);
+});
