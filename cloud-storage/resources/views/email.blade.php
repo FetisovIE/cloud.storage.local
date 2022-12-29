9f18559fda2430f234791f20a0b777a215c75bd6
Fetisov Igor <ig.fetisoff@gmail.com>
2022-12-29 08:07:11 +0300

    Creating a template to send to the mail

---
+++ cloud-storage/resources/views/email.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <style type="text/css">
+        a {
+            text-decoration: none;
+        }
+    </style>
+    <style>sup {
+            font-size: 100% !important;
+        }</style>
+    <xml>
+        <o:OfficeDocumentSettings>
+            <o:AllowPNG></o:AllowPNG>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+    </xml>
+</head>
+
+<body>
+<div class="es-wrapper-color">
+
+    <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+        <tbody>
+        <tr>
+            <td class="esd-email-paddings" valign="top">
+                <table cellpadding="0" cellspacing="0" class="es-content esd-header-popover" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe esd-synchronizable-module" align="center">
+                            <table class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600"
+                                   style="background-color: transparent;" bgcolor="rgba(0, 0, 0, 0)">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p20" align="left">
+                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                            <tbody>
+                                            <tr>
+                                                <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                    <table cellpadding="0" cellspacing="0" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td align="center" class="esd-empty-container"
+                                                                style="display: none;"></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table cellpadding="0" cellspacing="0" class="es-header" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe esd-synchronizable-module" align="center">
+                            <table bgcolor="#ffffff" class="es-header-body" align="center" cellpadding="0"
+                                   cellspacing="0" width="600">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p10t es-p10b es-p20r es-p20l" align="left">
+                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                            <tbody>
+                                            <tr>
+                                                <td width="560" class="es-m-p0r esd-container-frame" valign="top"
+                                                    align="center">
+                                                    <table cellpadding="0" cellspacing="0" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td align="center" class="esd-empty-container"
+                                                                style="display: none;"></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <table cellpadding="0" cellspacing="0" class="es-content" align="center">
+                    <tbody>
+                    <tr>
+                        <td class="esd-stripe" align="center">
+                            <table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0"
+                                   cellspacing="0" width="600">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-structure es-p30t es-p30b es-p20r es-p20l" align="left">
+                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                            <tbody>
+                                            <tr>
+                                                <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                    <table cellpadding="0" cellspacing="0" width="100%">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td align="center" class="esd-block-image es-p10t es-p10b"
+                                                                style="font-size: 0px;"><a target="_blank"><img
+                                                                        src="https://zjiocl.stripocdn.email/content/guids/CABINET_67e080d830d87c17802bd9b4fe1c0912/images/55191618237638326.png"
+                                                                        alt style="display: block;" width="100"></a>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center"
+                                                                class="esd-block-text es-p10b es-m-txt-c">
+                                                                <h1 style="font-size: 46px; line-height: 100%;">Reset
+                                                                    Your Password</h1>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center"
+                                                                class="esd-block-text es-p5t es-p5b es-p40r es-p40l es-m-p0r es-m-p0l">
+                                                                <p>You’ve received this message because your email
+                                                                    address has been registered with our site. Please
+                                                                    click the button below to verify your email address
+                                                                    and confirm that you are the owner of this
+                                                                    account.</p>
+                                                                {{ $link }}
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" class="esd-block-text es-p10t es-p5b">
+                                                                <p>If you did not register with us, please disregard
+                                                                    this email.</p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center"
+                                                                class="esd-block-text es-p5t es-p5b es-p40r es-p40l es-m-p0r es-m-p0l">
+                                                                <p>Once confirmed, this email will be uniquely
+                                                                    associated with your account.</p>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+
+</html>
